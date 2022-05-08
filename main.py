d7acce5b74a27f3302b71482de37bbcf5e27726d
Monoget <monoget2@gmail.com>
2022-05-08 18:12:12 +0600

    update code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ in_file_path = 'input/_metadata.json'
 
 col_name = []
 row_name=[]
-value=[]
+value_updated=[]
 json_object = []
 
 j = 1
@@ -26,14 +26,21 @@ with open(in_file_path, 'r') as in_json_file:
             if (result == -1):
                 col_name.append(json_o['trait_type'])
                 col_name.append('Total')
+
             n = len(row_name)
+
             result = search(row_name, n, [json_o['trait_type'],json_o['value']])
             if (result == -1):
                 row_name.append([json_o['trait_type'],json_o['value']])
-                value.append(1)
+                value_updated.append(1)
+                print([json_o['trait_type'], json_o['value'], value_updated[result],'new'])
             else:
-                print(result)
-                value.insert(result, float(value[result]+1))
+                print('old:'+str(value_updated[result]))
+                up=value_updated[result]+1
+                value_updated.insert(result, up)
+                print([json_o['trait_type'], json_o['value'], value_updated[result],'index: '+str(result),'old'])
+                print('new:'+str(value_updated[result]))
+
 
 
 
@@ -49,7 +56,7 @@ for x in range(len(col_name)):
     for y in range(len(row_name)):
         if row_name[y][0]==col_name[x]:
             worksheet.write(row, column, row_name[y][1])
-            worksheet.write(row, column+1, value[y])
+            worksheet.write(row, column+1, value_updated[y])
             row+=1
 
     column+=1
@@ -57,14 +64,16 @@ for x in range(len(col_name)):
 
 workbook.close()
 
-
-
 df = pd.read_excel('output/rarity.xlsx')
 
 print(df)
 
-print(len(col_name))
+print(len(col_name)/2)
 
 print(col_name)
-print(row_name)
-print(value)
+#i=0
+#for item in row_name:
+    #print(str(item)+' ['+str(value[i])+']')
+    #i+=1
+
+print(value_updated)
